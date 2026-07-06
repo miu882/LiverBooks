53233dba609b607edf80719b1ed96e56133359b3
miu882 <miu.10190202@outlook.jp>
2026-07-06 19:38:55 +0900

    Save receipt toggle

--- expen.js
+++ expen.js
@@ -66,3 +66,17 @@ function resetExpenseForm() {
     receiptImageInput.value = "";
   }
 }
+function setReceipt(value){
+
+    document.getElementById("receipt").value=value;
+
+    document.getElementById("receiptYes").classList.remove("active");
+    document.getElementById("receiptNo").classList.remove("active");
+
+    if(value==="あり"){
+        document.getElementById("receiptYes").classList.add("active");
+    }else{
+        document.getElementById("receiptNo").classList.add("active");
+    }
+
+}
